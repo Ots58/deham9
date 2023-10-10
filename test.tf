82936ab74c1e43bdda7afa8f1b98eb86238136d8
Temitope Solomon Odunsi <118396142+Ots58@users.noreply.github.com>
2023-10-10 06:55:00 +0200

    Create test.tf
    my first test

---
+++ test.tf
@@ -0,0 +1,16 @@
+module "ec2_instance" {
+  source  = "terraform-aws-modules/ec2-instance/aws"
+
+  name = "single-instance"
+
+  instance_type          = "t2.micro"
+  key_name               = "labsuser"
+  monitoring             = true
+  vpc_security_group_ids = ["sg-0c4d74681873f802a"]
+  subnet_id              = "subnet-*"
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
