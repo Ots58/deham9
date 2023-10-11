ebd5428efbd2d679c5f18b5fe59cd36bd2e52441
Temitope Solomon Odunsi <118396142+Ots58@users.noreply.github.com>
2023-10-11 17:06:26 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,14 +12,6 @@ terraform {
 #Provider profile and region in which all the resources will create
 provider "aws" {
   profile = "default"
-  region  = "eu-central-1"
+  region  = "us-west-2"
 }
 
-#Resource to create s3 bucket
-resource "aws_s3_bucket" "demo-bucket"{
-  bucket = "uniq587"
-
-  tags = {
-    Name = "S3Bucket"
-  }
-}
