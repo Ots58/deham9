99e32e9b5a67a3b0b58226463a3f29fed303f08f
Temitope Odunsi <odunsit85@gmail.com>
2023-10-12 17:24:09 +0200

    kj

--- main.tf
+++ main.tf
@@ -1,3 +1,24 @@
+# resource "aws_instance" "ec2_instance" {
+#   ami=var.AMIS[var.AWS_REGION] 
+#   instance_type = "t2.micro"
+#   associate_public_ip_address = true
+#   tags = {
+#     Name = "Terraform_test_ec2"
+#   }
+# }
+# output public_ip{
+#   value=aws_instance.ec2_instance.public_ip
+# }
+# resource "aws_instance" "webserver" {
+#   for_each = var.webservers
+#     ami=var.AMIS[var.AWS_REGION] 
+#     instance_type = "t2.micro"
+#     associate_public_ip_address = true
+#     tags = {
+#       Name = each.value["name"]
+#     }
+# }
+
 terraform {
   required_providers {
     aws = {
@@ -14,4 +35,27 @@ provider "aws" {
   profile = "default"
   region  = "us-west-2"
 }
+# Instance Configuration
+# Instance Configuration
+resource "aws_instance" "provisioner-remoteVM"{
+    ami = "ami-0d2017e886fc2c0ab"
+    instance_type = "t2.micro"
+    subnet_id = aws_subnet.public_subnet.id
+    tags = {
+        Name = "remote-instance"
+    }
+   } 
 
+  module "s32_bucket" {
+  source = "terraform-aws-modules/s32-bucket/aws"
+
+  bucket = "my-s32-bucket"
+  acl    = "private"
+
+  control_object_ownership = true
+  object_ownership         = "ObjectWriter"
+
+  versioning = {
+    enabled = true
+  }
+}
--- vpc.tf
+++ vpc.tf
@@ -1,5 +1,6 @@
 # VPC Creation using CIDR block available in vars.tf
 # VPC Creation using CIDR block available in vars.tf change we need
+data "aws_availability_zones" "available"{}
 
 resource "aws_vpc" "provisionerVPC"{
     cidr_block = var.vpc_cidr
@@ -11,3 +12,21 @@ resource "aws_vpc" "provisionerVPC"{
     }
 }
 
+# Public subnet public CIDR block available in vars.tf and provisionersVPC
+resource "aws_subnet" "public_subnet"{
+    cidr_block = var.vpc_cidr
+    vpc_id = aws_vpc.provisionerVPC.id
+    map_public_ip_on_launch = true
+    availability_zone = data.aws_availability_zones.available.names[1]
+    tags = {
+        Name = "dev-public-subnet"
+    }    
+}
+# To access EC2 instance inside a virtual private cloud we need an Internet Gateway and a routing table connecting the subnetto the Internet Gateway
+# Creating Internet Gateway
+resource "aws_internet_gateway" "gw"{
+    vpc_id = aws_vpc.provisionerVPC.id
+    tags = {
+        Name = "dev-gw"
+    }
+}
