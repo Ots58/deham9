5c77efe6fe78167c271cd435493e2fe7a0a494fc
Temitope Odunsi <odunsit85@gmail.com>
2023-10-13 16:00:07 +0200

    adding file to directory

--- main.tf
+++ main.tf
@@ -39,23 +39,35 @@ provider "aws" {
 # Instance Configuration
 resource "aws_instance" "provisioner-remoteVM"{
     ami = "ami-0d2017e886fc2c0ab"
-    instance_type = "t2.micro"
+    instance_type = var.instance_type
+    key_name = "vockey"
+    vpc_security_group_ids = [aws_security_group.dev_terraform_sg_allow_ssh_http.id]
     subnet_id = aws_subnet.public_subnet.id
     tags = {
         Name = "remote-instance"
     }
-   } 
 
-  module "s32_bucket" {
-  source = "terraform-aws-modules/s32-bucket/aws"
-
-  bucket = "my-s32-bucket"
-  acl    = "private"
-
-  control_object_ownership = true
-  object_ownership         = "ObjectWriter"
-
-  versioning = {
-    enabled = true
-  }
+    provisioner "remote-exec"{
+        inline = [
+            "sudo yum update -y",
+            "sudo yum install -y nginx",
+            "sudo service nginx start"
+        ]
+        on_failure = continue
+    }
+    
+    provisioner "local-exec"{
+        #ami=data.aws_ami.packeramis.id
+        #instance_type="t2.micro"
+        #when = "destroy"
+        command = "echo Instance Type=${self.instance_type},Instance ID=${self.id},Public DNS=${self.public_dns},AMI ID=${self.ami} >> allinstancedetails"
+    }
+connection {
+        type = "ssh"
+        host = aws_instance.provisioner-remoteVM.public_ip
+        user = "ec2-user"
+        private_key=file("${path.module}/vockey.pem")
+        
+    }
 }
+
--- vpc.tf
+++ vpc.tf
@@ -11,10 +11,9 @@ resource "aws_vpc" "provisionerVPC"{
         Name = "dev-terraform-vpc"
     }
 }
-
 # Public subnet public CIDR block available in vars.tf and provisionersVPC
 resource "aws_subnet" "public_subnet"{
-    cidr_block = var.vpc_cidr
+    cidr_block = var.public_cidr
     vpc_id = aws_vpc.provisionerVPC.id
     map_public_ip_on_launch = true
     availability_zone = data.aws_availability_zones.available.names[1]
@@ -29,4 +28,54 @@ resource "aws_internet_gateway" "gw"{
     tags = {
         Name = "dev-gw"
     }
+}
+# Public Route Table
+resource "aws_route_table" "public_route"{
+    vpc_id = aws_vpc.provisionerVPC.id
+    route {
+        cidr_block = var.cidr_blocks
+        gateway_id = aws_internet_gateway.gw.id
+    }
+    tags = {
+      Name = "dev-public-route"  
+    }
+}
+# Associate Public Subnet with Public Route Table
+resource "aws_route_table_association" "public_subnet_assoc"{
+    count = 1
+    route_table_id = aws_route_table.public_route.id
+    subnet_id = aws_subnet.public_subnet.id
+    depends_on = [aws_route_table.public_route,aws_subnet.public_subnet]    
+}
+# Security Group Creation for provisionerVPC
+resource "aws_security_group" "dev_terraform_sg_allow_ssh_http"{
+    name="dev-sg"
+    vpc_id = aws_vpc.provisionerVPC.id
+}
+# Ingress Security Port 22 (Inbound)
+resource "aws_security_group_rule" "ssh_ingress_access"{
+    from_port = 22
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 22
+    type = "ingress"
+    cidr_blocks = [var.cidr_blocks]
+}
+# Ingress Security Port 80 (Inbound)
+resource "aws_security_group_rule" "http_ingress_access"{
+    from_port = 80
+    protocol = "tcp"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 80
+    type = "ingress"
+    cidr_blocks = [var.cidr_blocks]
+}
+# All egress/outbound Access
+resource "aws_security_group_rule" "all_egress_access"{
+    from_port = 0
+    protocol = "-1"
+    security_group_id = aws_security_group.dev_terraform_sg_allow_ssh_http.id
+    to_port = 0
+    type = "egress"
+    cidr_blocks = [var.cidr_blocks]
 }
